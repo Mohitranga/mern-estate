1f9b538d2803ad1a2b03496480de918b04f390bd
Mohitranga <mohit.kumar90321@gmail.com>
2024-07-23 15:40:34 +0530

    create headeer component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import About from './pages/About';
+import Header from './component/Header';
 
 
 
@@ -12,9 +13,10 @@ import About from './pages/About';
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
           <Route path='/' element={<Home/>} />
-          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-in' element={<SignIn/>} />
           <Route path='/sign-up' element={<SignUp/>}/>
           <Route path='/Profile' element={<Profile/>}/>
           <Route path='/About' element={<About/>}/>
