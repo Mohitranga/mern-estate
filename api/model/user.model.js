c3ca974e54c09d46523d932e4f9659c17ec66d15
Mohitranga <mohit.kumar90321@gmail.com>
2024-07-24 19:24:26 +0530

    create user model

---
+++ api/model/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+/// creation of schemma
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        
+    }
+},
+    {timestamps:true});
+/// creation of model
+ const user=mongoose.model('User',userSchema)
+
+ export default User;
