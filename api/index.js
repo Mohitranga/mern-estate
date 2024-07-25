e28d314f9ac54d400bf15da60cfd9736f964adcb
Mohitranga <mohit.kumar90321@gmail.com>
2024-07-25 17:56:24 +0530

    create a middle wear and  a function to handle possible error

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotnev from 'dotenv';
 import userRouter from  './routes/user.route.js';
+import authRouter from   './routes/auth.route.js';
+
 dotnev.config();
 
 
@@ -13,6 +15,7 @@ mongoose
     })
 
 const app=express();
+app.use(express.json());
 app.listen(3000,()=>
 {
     console.log('server is running on port 3000');
@@ -20,3 +23,4 @@ app.listen(3000,()=>
 
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
